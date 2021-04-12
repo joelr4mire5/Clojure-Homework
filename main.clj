71f7644203da056a437cd6af9a2a2507bd6ea1c7
Joel David <>
2021-04-12 02:40:03 +0000

    Initial commit

---
+++ main.clj
@@ -0,0 +1,61 @@
+(defn funclineal [m b x]
+  (+(* m x) b)
+  )
+
+(funclineal 4 6 5)
+
+
+
+(defn pendiente [x1 y1 x2 y2]
+(cond 
+  (== (/(- y2 y1)(- x2 x1)) 0) "Constante"
+  (> (/(- y2 y1)(- x2 x1)) 0) "Creciente"
+  (< (/(- y2 y1)(- x2 x1)) 0) "Decreceinte"
+)
+)
+
+( pendiente 0 3 1 3)
+
+
+(defn pendientem [x1 y1 x2 y2]
+  (/(- y2 y1)(- x2 x1))  
+)
+
+(pendientem 0 3 1 3)
+
+
+
+(defn funcion-punto-recta [m b x y]
+(cond
+  (= (funclineal m b x) y) "El punto esta en la recta"
+  :else "El punto NO esta en la recta")
+)
+
+(funcion-punto-recta 6 7 9 2)
+
+(defn funclinealb [m x y]
+  (- y (* m x))
+  )
+
+(funclinealb 4 5 26)
+
+(defn funcion-m-b [x1 y1 x2 y2]
+
+  (funclinealb (  x1 y1  )
+  )
+
+
+(funcion-m-b 0 3 1 3)
+
+
+
+
+
+
+
+
+
+
+
+
+
